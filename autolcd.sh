b716cf107ebfdf074ac9316d60d103128a649d25
zhoubin <ulvge@126.com>
2024-12-29 16:07:13 +0800

    no bug

--- autolcd.sh
+++ autolcd.sh
@@ -3,10 +3,15 @@
 trap "echo 'Service stopping...'; kill 0; exit" SIGTERM
 
 # define vars
-TIMEOUT=30 # 
+TIMEOUT=60 # 
 SCREEN_OFF_CMD="setterm --blank force --term linux < /dev/tty1"
 SCREEN_ON_CMD="setterm --blank poke --term linux < /dev/tty1"
 DPMS_PATH="/sys/class/drm/card0-LVDS-1/dpms"
+
+last_activity=$(date +%s)
+# last_activity
+LAST_ACTIVITY_FILE="/tmp/last_activity"
+
 
 screen_status() {
     if [[ $(cat $DPMS_PATH) == "Off" ]]; then
@@ -18,12 +23,12 @@ screen_status() {
 # monitor 
 monitor_keyboard() {
     while read -r event; do
-        current_time=$(date +%s)
-        
+        last_act=$(date +%s)
+        echo "$last_act" > $LAST_ACTIVITY_FILE
+        # logger "monitor last_activity: $last_act"
         # if lcd is OFF ,then ON
         if [[ $screen_status -eq 0 ]]; then
             logger "Screen is off -> on"
-            last_activity=$current_time
             eval "$SCREEN_ON_CMD"
         fi
     done < <(stdbuf -oL evtest /dev/input/event0 2>/dev/null | grep --line-buffered "value 1")
@@ -32,15 +37,21 @@ monitor_keyboard() {
 # main check
 check_timeout() {
     while true; do
-        sleep 1
+        sleep 3
         current_time=$(date +%s)
+
+        # last_activity
+        if [[ -f $LAST_ACTIVITY_FILE ]]; then
+            last_activity=$(cat $LAST_ACTIVITY_FILE)
+        fi
+
         elapsed=$((current_time - last_activity))
         #logger "screen Checking timeout: $elapsed seconds elapsed."
+        #logger "last_activity: $last_activity"
 
         # if no key & lcd is ON, then OFF
         if [[ $elapsed -ge $TIMEOUT && $(screen_status) -eq 1 ]]; then
             logger "Screen is on -> off"
-            last_activity=$(date +%s)
             eval "$SCREEN_OFF_CMD"
         fi
     done
